af7f86b75ed8e4c739627754f69d4bbb20825603
Vadim <roziznanvadim@mail.ru>
2023-03-10 20:22:12 +0300

    10.03.2023-2

--- 5.Regexp/main.py
+++ 5.Regexp/main.py
@@ -5,14 +5,17 @@ import re
 with open("phonebook_raw.csv", encoding='utf-8') as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
-    pattern = r"(\s+'',)/(\')"
+    pattern = r"('|\n)"
     replace = r""
-
+    pattern_2 = r"\s+"
+    replace_2 = r" "
     r = []
-    for el in contacts_list:
+    for index, item in enumerate(contacts_list):
 
-        r.append(re.sub(pattern, replace, ' '.join(el)))
-
+        r.append(re.sub(pattern, replace, ' '.join(contacts_list[index])))
+        r[index] = re.sub(pattern_2, replace_2, r[index])
+        # print(r[index].split()[0:2])
     pprint(r)
+
 
 
